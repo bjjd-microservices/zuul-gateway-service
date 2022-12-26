457d7613c49a524a54a1f3f547c6a1a701b8126c
rajivbansal2981 <rajivbansal2981@gmail.com>
2022-12-26 15:16:46 +0530

    JaiMataKi

--- src/main/java/com/jmk/gateway/GatewayServiceApplication.java
+++ src/main/java/com/jmk/gateway/GatewayServiceApplication.java
@@ -1,32 +1,23 @@
 package com.jmk.gateway;
 
-import java.util.stream.Stream;
-
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.jmk.gateway.ribbon.config.RibbonConfiguration;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import java.util.stream.Stream;
 
 @EnableDiscoveryClient  // It acts as a eureka client
 @EnableZuulProxy     // Enable Zuul
 @EnableSwagger2
-@AutoConfigureAfter(RibbonAutoConfiguration.class)
-@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @ComponentScan(basePackages = { "com.jmk" })
 @SpringBootApplication
 public class GatewayServiceApplication {
